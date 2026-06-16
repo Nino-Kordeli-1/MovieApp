7ae0bbf6440a9383e4d7f86746c82da675847099
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-06-16 18:24:31 +0400

    Removed empty spaces.

--- app/src/main/java/com/movieapp/ui/theme/Color.kt
+++ app/src/main/java/com/movieapp/ui/theme/Color.kt
@@ -8,9 +8,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-
 val YellowPrimary = Color(0xFFFFC44A)
-
 val Neutral01Black = Color(0xFF080808)
 val Neutral02DarkestGrey = Color(0xFF1C1C1C)
 val Neutral03DarkGrey = Color(0xFF5D5D5D)
