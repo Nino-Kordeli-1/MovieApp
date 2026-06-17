b2a379059db9c98845e98892c75f4c72d6885d59
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-06-17 13:39:13 +0400

    Added dependencies.

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -15,4 +15,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    implementation(libs.androidx.compose.material3)
 }
