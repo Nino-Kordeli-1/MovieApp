51d8d9b98406d9618c9ec941aa1b55b3a15eb836
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-06-17 11:28:46 +0400

    Added spacings in design system.

---
+++ core/designsystem/src/main/java/com/designsystem/Spacing.kt
@@ -0,0 +1,25 @@
+package com.designsystem
+
+import androidx.compose.ui.unit.dp
+
+object Spacing {
+    val spacing_2 = 2.dp
+    val spacing_4 = 4.dp
+    val spacing_6 = 6.dp
+    val spacing_8 = 8.dp
+    val spacing_10 = 10.dp
+    val spacing_12 = 12.dp
+    val spacing_14 = 14.dp
+    val spacing_16 = 16.dp
+    val spacing_18 = 18.dp
+    val spacing_20 = 20.dp
+    val spacing_22 = 22.dp
+    val spacing_24 = 24.dp
+    val spacing_26 = 26.dp
+    val spacing_28 = 28.dp
+    val spacing_30 = 30.dp
+    val spacing_32 = 32.dp
+    val spacing_34 = 34.dp
+    val spacing_36 = 36.dp
+    val spacing_110 = 110.dp
+}
