feeb23fc68c747655890d4379f8a05ff95332c73
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-06-19 02:03:09 +0400

    Added resource mapper and made changes in network result class.

--- core/common/src/main/java/com/common/mapper/ResourceMapper.kt
+++ core/common/src/main/java/com/common/mapper/ResourceMapper.kt
@@ -7,11 +7,22 @@ import kotlinx.coroutines.flow.map
 suspend fun <Dto : Any, Domain : Any> Flow<NetworkResult<Dto>>.asResource(
     onSuccess: suspend (Dto) -> Domain,
 ): Flow<NetworkResult<Domain>> {
-    return this.map {
-        when (it) {
-            is NetworkResult.Error<*> -> TODO()
-            is NetworkResult.Loading<*> -> TODO()
-            is NetworkResult.Success<*> -> TODO()
+    return this.map { result ->
+        when (result) {
+            is NetworkResult.Error -> NetworkResult.Error(
+                errorMessage = result.errorMessage,
+                throwable = result.throwable
+            )
+
+            NetworkResult.Loading -> NetworkResult.Loading
+            is NetworkResult.Success -> try {
+                NetworkResult.Success(onSuccess(result.data))
+            } catch (e: Exception) {
+                NetworkResult.Error(
+                    errorMessage = e.message ?: "Mapping failed",
+                    throwable = e
+                )
+            }
         }
     }
 }
--- core/common/src/main/java/com/common/resource/NetworkResult.kt
+++ core/common/src/main/java/com/common/resource/NetworkResult.kt
@@ -2,6 +2,9 @@ package com.common.resource
 
 sealed class NetworkResult<out D : Any> {
     data class Success<out D : Any>(val data: D) : NetworkResult<D>()
-    data class Loading<Nothing : Any>(val loading: Boolean) : NetworkResult<Nothing>()
-    data class Error<out D : Any>(val errorMessage: String) : NetworkResult<D>()
+    data object Loading : NetworkResult<Nothing>()
+    data class Error(
+        val errorMessage: String,
+        val throwable: Throwable? = null
+    ) : NetworkResult<Nothing>()
 }
