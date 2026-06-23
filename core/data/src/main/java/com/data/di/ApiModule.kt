5269bfd183970f55809685753d36a646c80b1ca8
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-06-23 17:41:48 +0400

    Registered another dependency in api module

--- core/data/src/main/java/com/data/di/ApiModule.kt
+++ core/data/src/main/java/com/data/di/ApiModule.kt
@@ -1,11 +1,15 @@
 package com.data.di
 
 import com.data.remote.PopularMovieApi
+import com.data.remote.SearchAndGenreApi
 import org.koin.dsl.module
 import retrofit2.Retrofit
 
 val apiModule = module {
     single<PopularMovieApi> {
         get<Retrofit>().create(PopularMovieApi::class.java)
+    }
+    single<SearchAndGenreApi> {
+        get<Retrofit>().create(SearchAndGenreApi::class.java)
     }
 }
