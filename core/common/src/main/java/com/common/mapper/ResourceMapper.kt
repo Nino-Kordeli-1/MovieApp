a176ebda6c0a5dadc8d3897becb1f2315ff72b31
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-06-23 17:58:36 +0400

    Modified resource mapper

--- core/common/src/main/java/com/common/mapper/ResourceMapper.kt
+++ core/common/src/main/java/com/common/mapper/ResourceMapper.kt
@@ -4,19 +4,25 @@ import com.common.resource.NetworkResult
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-suspend fun <Dto : Any, Domain : Any> Flow<NetworkResult<Dto>>.asResource(
-    onSuccess: suspend (Dto) -> Domain,
+fun <Dto : Any, Domain : Any> Flow<NetworkResult<Dto>>.asResource(
+    mapper: suspend (Dto) -> Domain
 ): Flow<NetworkResult<Domain>> {
     return this.map { result ->
         when (result) {
-            is NetworkResult.Error -> NetworkResult.Error(
-                errorMessage = result.errorMessage,
-                throwable = result.throwable
-            )
 
-            NetworkResult.Loading -> NetworkResult.Loading
+            is NetworkResult.Loading ->
+                NetworkResult.Loading
+
+            is NetworkResult.Error ->
+                NetworkResult.Error(
+                    errorMessage = result.errorMessage,
+                    throwable = result.throwable
+                )
+
             is NetworkResult.Success -> try {
-                NetworkResult.Success(onSuccess(result.data))
+                NetworkResult.Success(
+                    mapper(result.data)
+                )
             } catch (e: Exception) {
                 NetworkResult.Error(
                     errorMessage = e.message ?: "Mapping failed",
