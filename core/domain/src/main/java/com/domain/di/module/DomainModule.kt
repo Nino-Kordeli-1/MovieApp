fd4b90aa784e193b78a8748799991abacf79fdd7
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-06-24 18:27:18 +0400

    Moved files

--- core/domain/src/main/java/com/domain/di/module/DomainModule.kt
+++ core/domain/src/main/java/com/domain/di/module/DomainModule.kt
@@ -1,8 +1,10 @@
-package com.data.di
+package com.domain.di.module
 
+import com.domain.usecase.GetGenresUseCase
 import com.domain.usecase.GetPopularMoviesUseCase
 import org.koin.dsl.module
 
 val domainModule = module {
     factory { GetPopularMoviesUseCase(repository = get()) }
+    factory { GetGenresUseCase(repository = get()) }
 }
