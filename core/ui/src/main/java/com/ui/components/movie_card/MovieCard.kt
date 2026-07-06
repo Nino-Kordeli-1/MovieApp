910499f788aa80b3bbf12eb8b2aa2a8f05759079
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-07-06 09:19:45 +0400

    Added shimmer

--- core/ui/src/main/java/com/ui/components/movie_card/MovieCard.kt
+++ core/ui/src/main/java/com/ui/components/movie_card/MovieCard.kt
@@ -1,9 +1,11 @@
 package com.ui.components.movie_card
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,11 +18,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 import com.designsystem.Spacing
 import com.designsystem.theme.Neutral08Whisper
 import com.designsystem.theme.NeutralGrey04Grey
@@ -48,13 +51,29 @@ fun MovieCard(
         ) {
             Box {
 
-                AsyncImage(
+                SubcomposeAsyncImage(
                     model = posterUrl,
                     contentDescription = title,
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(Spacing.spacing_226),
-                    contentScale = ContentScale.Crop
+                    contentScale = ContentScale.Crop,
+
+                    loading = {
+                        Box(
+                            Modifier
+                                .fillMaxSize()
+                                .shimmer()
+                        )
+                    },
+
+                    error = {
+                        Box(
+                            Modifier
+                                .fillMaxSize()
+                                .background(Color.DarkGray)
+                        )
+                    }
                 )
 
                 CategoryLabel(
