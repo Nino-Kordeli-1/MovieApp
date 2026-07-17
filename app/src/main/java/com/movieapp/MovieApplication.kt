471b5ab35004d73ac29d779f2ffa56b2b7076d35
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-07-17 14:38:59 +0400

    Registered modules

--- app/src/main/java/com/movieapp/MovieApplication.kt
+++ app/src/main/java/com/movieapp/MovieApplication.kt
@@ -2,8 +2,13 @@ package com.movieapp
 
 import android.app.Application
 import com.data.di.coreModules
+import com.data.di.dataModule
+import com.data.di.observerModule
+import com.impl.di.detailsModule
 import com.impl.di.homeModule
+import com.impl.screen.favorites.di.favoritesModule
 import com.impl.screen.splash.module.splashModule
+import com.movieapp.di.databaseModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 
@@ -17,6 +22,11 @@ class MovieApplication : Application() {
                 coreModules + listOf(
                     homeModule,
                     splashModule,
+                    databaseModule,
+                    dataModule,
+                    favoritesModule,
+                    detailsModule,
+                    observerModule
                 )
             )
         }
