8c9d3e4bb00dd9ad6156333a4a31e7142d051345
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-07-21 17:40:38 +0400

    Fixed updateState and added navigation functions to baseVm

--- core/ui/src/main/java/com/ui/base/vm/BaseViewModel.kt
+++ core/ui/src/main/java/com/ui/base/vm/BaseViewModel.kt
@@ -2,10 +2,12 @@ package com.ui.base.vm
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.navigation.NavCommand
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 abstract class BaseViewModel<State, Event, SideEffect>(
@@ -18,15 +20,24 @@ abstract class BaseViewModel<State, Event, SideEffect>(
     private val _sideEffect = MutableSharedFlow<SideEffect>()
     val sideEffect = _sideEffect.asSharedFlow()
 
+    private val _navigationCommands = MutableSharedFlow<NavCommand>()
+    val navigationCommands = _navigationCommands.asSharedFlow()
+
     open fun onEvent(event: Event) {}
 
     protected fun updateState(update: (State) -> State) {
-        _state.value = update(_state.value)
+        _state.update(update)
     }
 
     protected fun emitSideEffect(sideEffect: SideEffect) {
         viewModelScope.launch {
             _sideEffect.emit(sideEffect)
+        }
+    }
+
+    protected fun navigate(command: NavCommand) {
+        viewModelScope.launch {
+            _navigationCommands.emit(command)
         }
     }
 }
