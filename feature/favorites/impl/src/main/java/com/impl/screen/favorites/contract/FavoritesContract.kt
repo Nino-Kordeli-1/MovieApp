b3e353af4ae2de57eb305abcb27c5bfe0792a251
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-07-31 11:16:41 +0400

    Made change in FavoritesContract

--- feature/favorites/impl/src/main/java/com/impl/screen/favorites/contract/FavoritesContract.kt
+++ feature/favorites/impl/src/main/java/com/impl/screen/favorites/contract/FavoritesContract.kt
@@ -1,20 +1,25 @@
 package com.impl.screen.favorites.contract
 
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.model.MovieUiModel
 import com.domain.model.GenreResponse
+import com.domain.model.MovieResponse
 
 data class FavoritesUiState(
     val favorites: List<MovieUiModel> = emptyList(),
     val genreList: List<GenreResponse> = emptyList(),
     val isLoading: Boolean = false,
-    val error: String? = null
-)
+    val error: String? = null,
+    var movies: List<MovieResponse> = emptyList()
+) {
+    var gridState: LazyGridState by mutableStateOf(LazyGridState(0, 0))
+        private set
+}
 
 sealed interface FavoritesUiEvent {
     data class RemoveFavorite(val movieId: Int) : FavoritesUiEvent
     data class MovieClicked(val movieId: Int) : FavoritesUiEvent
-}
-
-sealed interface FavoritesUiSideEffect {
-    data class NavigateToDetails(val movieId: Int) : FavoritesUiSideEffect
 }
