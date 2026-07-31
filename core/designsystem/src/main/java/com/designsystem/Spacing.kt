f0443d4ed3fcfa2933a2472f2bec732aac0d010d
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-07-31 11:07:10 +0400

    Made changes in spacings

--- core/designsystem/src/main/java/com/designsystem/Spacing.kt
+++ core/designsystem/src/main/java/com/designsystem/Spacing.kt
@@ -5,32 +5,25 @@ import androidx.compose.ui.unit.dp
 object Spacing {
     val spacing_0 = 0.dp
     val spacing_0_5 = 0.5.dp
-    val spacing_1 = 1.dp
     val spacing_2 = 2.dp
     val spacing_4 = 4.dp
     val spacing_6 = 6.dp
     val spacing_8 = 8.dp
     val spacing_10 = 10.dp
     val spacing_12 = 12.dp
-    val spacing_14 = 14.dp
     val spacing_16 = 16.dp
     val spacing_18 = 18.dp
     val spacing_20 = 20.dp
     val spacing_22 = 22.dp
     val spacing_24 = 24.dp
-    val spacing_26 = 26.dp
     val spacing_28 = 28.dp
-    val spacing_30 = 30.dp
-    val spacing_32 = 32.dp
     val spacing_34 = 34.dp
     val spacing_36 = 36.dp
     val spacing_38 = 38.dp
-    val spacing_50 = 50.dp
+    val spacing_44 = 44.dp
+    val spacing_56 = 56.dp
     val spacing_63 = 63.dp
-    val spacing_110 = 110.dp
-    val spacing_120 = 120.dp
-    val spacing_140 = 140.dp
+    val spacing_134 = 134.dp
     val spacing_160 = 160.dp
-    val spacing_200 = 200.dp
     val spacing_226 = 226.dp
 }
