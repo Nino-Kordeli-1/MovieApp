844fdb9fa3859442389e951ec3d5fb82fe544ca0
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-07-31 10:30:30 +0400

    Made changes in details contract

--- feature/details/impl/src/main/java/com/impl/screen/details/contract/DetailsContract.kt
+++ feature/details/impl/src/main/java/com/impl/screen/details/contract/DetailsContract.kt
@@ -1,11 +1,15 @@
 package com.impl.screen.details.contract
 
+import com.domain.model.GenreResponse
+import com.domain.model.MovieDetails
 import com.model.MovieDetailsUiModel
 
 data class DetailsUiState(
     val movie: MovieDetailsUiModel? = null,
     val isLoading: Boolean = false,
-    val error: String? = null
+    val error: String? = null,
+    var genres: List<GenreResponse> = emptyList(),
+    var currentMovie: MovieDetails? = null
 )
 
 sealed interface DetailsUiEvent {
